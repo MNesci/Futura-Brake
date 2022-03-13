8344df3909318f3775ea8f5b927c97672bde9516
Mike Nesci <mikenesci@hotmail.com>
2022-03-13 16:18:42 -0400

    Restructure layout and add home page image slider
    Resturctured the layout to a multi-page site, and added an image slider to the home page. Added script.js with pseudo-code for the slider functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+// Slider
+
+// assign each slide to a variable. or assign a variable for an array (from node list) of them
+
+
+// assign each arrow to a variable
+
+
+// assign each button to a variable
+
+
+
+// function that adds Hidden attribute to current slide, and removes it from next slide. probably have to increment an iterator
+
+
+// function that adds Hidden attribute to current slide, and removes it from previous slide. probably have to increment an iterator
+
+
+// functions that adds Hidden to other slides, and removes it from slide associated with button clicked
+
