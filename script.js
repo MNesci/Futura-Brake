7a1135821d9732ef6c7a83c1244f4e9c7393242a
Mike Nesci <mikenesci@hotmail.com>
2022-03-15 20:07:34 -0400

    Add fade animation for slider, and reset automated slider when a button is pressed
    Added a fade animation when slides change using the opacity CSS property instead of the hidden attribute. Used the clearInterval() function to reset the automated slider when a slide is manually changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Slider
+// Slider with fade transition animation using opacity property in CSS
 
 // Slide Array
 const slideArray = Array.from(document.querySelectorAll('.slide'));
@@ -15,41 +15,55 @@ const buttonArray = Array.from(document.querySelectorAll('.slideButton'));
 let indexOfPreviouslyVisibleSlide;
 let indexOfNextSlide;
 
-// RIGHT ARROW
-rightArrow.addEventListener('click', showNextSlide());
 
-// function that adds Hidden attribute to current slide, and removes it from next slide. probably have to increment an iterator
+// function that adds hideSlide class to current slide, and removes it from next slide. probably have to increment an iterator
 function showNextSlide() {
-    // function that checks slides for hidden, and saves the index of the slide that isn't hidden
+    // function that checks slides for .hideSlide, and saves the index ofthe    slide that isn't hidden
     slideArray.forEach(function(slide) {
-        if (!slide.hasAttribute('hidden')) {
+        if (!slide.classList.contains('hideSlide')) {
             indexOfPreviouslyVisibleSlide = slideArray.indexOf(slide);
         };
     });
     // set slide of that index to hidden
-    slideArray[indexOfPreviouslyVisibleSlide].setAttribute('hidden', 'hidden');
-
-    // compare index to array length. if last slide, set next slide to first. otherwise, set next to next slide
+    slideArray[indexOfPreviouslyVisibleSlide].classList.add('hideSlide');
+    // compare index to array length. if last slide, set next slide tofirst.    otherwise, set next to next slide
     if (indexOfPreviouslyVisibleSlide === slideArray.length - 1) {
         indexOfNextSlide = 0;
     } else {
         indexOfNextSlide = indexOfPreviouslyVisibleSlide + 1;
     };
     // set slide to unhidden
-    slideArray[indexOfNextSlide].removeAttribute('hidden');
-}
+    slideArray[indexOfNextSlide].classList.remove('hideSlide');
+};
+
+
+// Automated Scrolling using function that Right Arrow Event Listener uses
+// Assigns a variable to the interval ID return of automated scrolling
+let intervalId = setInterval(showNextSlide, 5000);
+
+
+// RIGHT ARROW
+rightArrow.addEventListener('click', function() {
+    showNextSlide();
+    // Clear automated scrolling interval
+    clearInterval(intervalId);
+    // Start automated scrolling interval again
+    // Because of scoping, must update value of intervalId
+    intervalId = setInterval(showNextSlide, 5000);
+});
+
 
 // LEFT ARROW
 // function that adds Hidden attribute to current slide, and removes it from previous slide. probably have to increment an iterator
 leftArrow.addEventListener('click', function(click) {
-    // function that checks slides for hidden, and saves the index of the slide that isn't hidden
+    // function that checks slides for .hideSlide, and saves the index of the slide that isn't hidden
     slideArray.forEach(function(slide) {
-        if (!slide.hasAttribute('hidden')) {
+        if (!slide.classList.contains('hideSlide')) {
             indexOfPreviouslyVisibleSlide = slideArray.indexOf(slide);
         };
     });
     // set slide of that index to hidden
-    slideArray[indexOfPreviouslyVisibleSlide].setAttribute('hidden', 'hidden');
+    slideArray[indexOfPreviouslyVisibleSlide].classList.add('hideSlide');
 
     // compare index to array length. if first slide, set next slide to last. otherwise, set next to previous slide
     if (indexOfPreviouslyVisibleSlide === 0) {
@@ -58,25 +72,30 @@ leftArrow.addEventListener('click', function(click) {
         indexOfNextSlide = indexOfPreviouslyVisibleSlide - 1;
     };
     // set slide to unhidden
-    slideArray[indexOfNextSlide].removeAttribute('hidden');
+    slideArray[indexOfNextSlide].classList.remove('hideSlide');
+    // Clear automated scrolling interval
+    clearInterval(intervalId);
+    // Start automated scrolling interval again
+    // Because of scoping, must update value of intervalId
+    intervalId = setInterval(showNextSlide, 5000);
 });
 
 
-// functions that adds Hidden to other slides, and removes it from slide associated with button clicked
+// functions that adds .hideSlide to other slides, and removes it from slide associated with button clicked
 
 buttonArray.forEach(function(button) {
     button.addEventListener('click', function(click) {
         slideArray.forEach(function(slide) {
-            slide.setAttribute('hidden', 'hidden');
+            slide.classList.add('hideSlide');
         });
         let indexOfButtonClicked = buttonArray.indexOf(button);
-        slideArray[indexOfButtonClicked].removeAttribute('hidden');
+        slideArray[indexOfButtonClicked].classList.remove('hideSlide');
+        // Clear automated scrolling interval
+        clearInterval(intervalId);
+        // Start automated scrolling interval again
+        // Because of scoping, must update value of intervalId
+        intervalId = setInterval(showNextSlide, 5000);
     });
 });
 
-
-// Automated Scrolling using function that Right Arrow Event Listener uses
-setInterval(showNextSlide, 5000);
-
-
-// FIGURE OUT ANIMATION OF SOME KIND (FLIP OR SCROLL MAYBE)
+// interval clear and reset on buttons doesn't work the second time a button is clicked
